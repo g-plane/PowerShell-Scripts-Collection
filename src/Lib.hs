9e16f76724642990ac279cff78f4759874d708e7
Pig Fang <g-plane@hotmail.com>
2020-07-10 15:38:12 +0800

    refactor

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,21 +4,26 @@ module Lib
   )
 where
 
+import           Data.Function
 import qualified Data.Text                     as T
 import           System.IO                      ( FilePath )
 
 names :: [FilePath] -> [String]
-names =
-  map (T.unpack . (flip T.append) (T.pack ".ass") . T.dropEnd 4)
-    . filter
+names xs =
+  xs
+    & map T.pack
+    & filter
         (\x -> T.isSuffixOf (T.pack ".mp4") x || T.isSuffixOf (T.pack ".mkv") x)
-    . map T.pack
+    & map (T.dropEnd 4)
+    & map (flip T.append (T.pack ".ass"))
+    & map T.unpack
 
 subtitles :: [FilePath] -> [FilePath]
-subtitles =
-  map T.unpack
-    . filter
+subtitles xs =
+  xs
+    & map T.pack
+    & filter
         (\x -> T.isSuffixOf (T.pack ".ass") x
           && not (T.isInfixOf (T.pack "cht") x || T.isInfixOf (T.pack "TC") x)
         )
-    . map T.pack
+    & map T.unpack
