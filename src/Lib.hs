4c4cf51359c31482d5e2d3d2168376f6a9e99f48
Pig Fang <g-plane@hotmail.com>
2020-07-11 16:54:58 +0800

    refactor: use `extra` instead of `text`

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,25 +5,17 @@ module Lib
 where
 
 import           Data.Function
-import qualified Data.Text                     as T
+import           Data.List
+import           Data.List.Extra
 import           System.IO                      ( FilePath )
 
 names :: [FilePath] -> [String]
 names xs =
   xs
-    & map T.pack
-    & filter
-        (\x -> T.isSuffixOf (T.pack ".mp4") x || T.isSuffixOf (T.pack ".mkv") x)
-    & map (T.dropEnd 4)
-    & map (`T.append` T.pack ".ass")
-    & map T.unpack
+    & filter (\x -> isSuffixOf ".mp4" x || isSuffixOf ".mkv" x)
+    & map (dropEnd 4)
+    & map (++ ".ass")
 
 subtitles :: [FilePath] -> [FilePath]
-subtitles xs =
-  xs
-    & map T.pack
-    & filter
-        (\x -> T.isSuffixOf (T.pack ".ass") x
-          && not (T.isInfixOf (T.pack "cht") x || T.isInfixOf (T.pack "TC") x)
-        )
-    & map T.unpack
+subtitles = filter
+  (\x -> isSuffixOf ".ass" x && not (isInfixOf "cht" x || isInfixOf "TC" x))
