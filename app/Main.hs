00578f9290e65d045436b5be051050f834373ab7
Pig Fang <g-plane@hotmail.com>
2021-07-17 19:23:30 +0800

    clean up

--- app/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import           Lib
-import           System.Directory
-
-main :: IO ()
-main = do
-  entries <- listDirectory "."
-  mapM_ (uncurry renameFile) $ zip (subtitles entries) (names entries)
